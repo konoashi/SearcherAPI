f808da6fe2e6affbe1ebceafa1d215da5194ecdd
Muradore Guilhem <64316846+konoashi@users.noreply.github.com>
2022-06-11 18:51:52 +0200

    Avancé sur le compteur de keys

--- src/fr/konoashi/searcher/Main.java
+++ src/fr/konoashi/searcher/Main.java
@@ -1,7 +1,23 @@
 package fr.konoashi.searcher;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
+
+
+public class Main {
+    public static String[] keys = {"a74dfce5-f68c-4405-a0b6-c3607ad6f30b", "ea4095c4-3dcf-4676-9b37-f6756e99bd22"};
+    public static void main(String[] args) {
+        ConcurrentHashMap<UUID, Integer> m
+                = new ConcurrentHashMap<>();
+
+        for (int i = 1; i < keys.length+1; i++) {
+            m.put(UUID.fromString(keys[i-1]), 0);
+        }
+        System.out.println(m);
     }
 }
